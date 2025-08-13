c31eb722c320b86daee5631a50398775dc8273ce
NarimanOusman <narimanrobo8@gmail.com>
2025-08-13 22:08:46 +0200

    updated the nav bar by adding black background when scrolY is 80, used api key and id from TMDB to get the movie lists

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.svg";
@@ -7,8 +7,18 @@ import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
 
 const Navbar = () => {
+  const nav = useRef();
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= 80) {
+        nav.current.classList.add("nav_dark");
+      } else {
+        nav.current.classList.remove("nav_dark");
+      }
+    });
+  }, []);
   return (
-    <div className="navbar">
+    <div ref={nav} className="navbar">
       <div className="navbar_left">
         <img src={logo} alt="" />
         <ul>
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,14 +1,34 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import Cards_data from "../../assets/cards/Cards_data.js";
+import { Link } from "react-router-dom";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
-  const handleWheel = (event) => {
-    event.preventDefault();
-    cardsRef.current.scrollLeft += event.deltaY;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGMwNmI3ZjczZTg0ZDRlNDFlNTgwZWZhY2Y3MDRkMiIsIm5iZiI6MTc1NDc2NDMzNS44OTUsInN1YiI6IjY4OTc5NDJmZjY3OWYxZjk4MDhlYTg3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dblBYNK6bh2Bu9ohWEvkocaHxF9T14Jn354dmeitkHk",
+    },
   };
+
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+    const handleWheel = (event) => {
+      event.preventDefault();
+      cardsRef.current.scrollLeft += event.deltaY;
+    };
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
 
@@ -16,12 +36,17 @@ const TitleCards = ({ title, category }) => {
     <div className="titlecards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card_list" ref={cardsRef}>
-        {Cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
-            </div>
+            <Link to={`/Player/${card.id}`} key={index}>
+              <div className="card">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                  alt=""
+                />
+                <p>{card.original_title}</p>
+              </div>
+            </Link>
           );
         })}
       </div>
